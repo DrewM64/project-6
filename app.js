2c11a6421f6812c7797a2bfed9fddc402e97a806
DrewM64 <74701517+DrewM64@users.noreply.github.com>
2021-03-19 16:54:01 -0400

    Add btn__reset event listener

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-const buttonReset = document.getElementsByClassName('btn_reset');
+const buttonReset = document.getElementsByClassName('btn__reset')[0];
 let missed = 0;
+const phrases = ["eat hot chip", 'go tuck yourself in', 'rubber babies', 'hey arnold', 'the things i do for love'];
+
+buttonReset.addEventListener('click', () => {
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = "none";
+    });
+
