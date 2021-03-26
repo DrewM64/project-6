d73cac3d5d87b3ba9328d9f2cec5343a1430487d
DrewM64 <74701517+DrewM64@users.noreply.github.com>
2021-03-25 21:48:58 -0400

    feat: Add checkWin() function
    -Added checkWin() to check whether the user wins or loses the game on each button click
    -Added event listener to check when a letter has already been choasen
    -Styled letters to fade into view upon reveal

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const overlay = document.getElementById('overlay');
+const heading = overlay.getElementsByTagName("h2")[0];
 const buttonReset = document.getElementsByClassName('btn__reset')[0];
 let missed = 0;
+const scoreboard = document.getElementById("scoreboard").firstElementChild;
 const phrases = ["eat hot chip", 
                 'go tuck yourself in', 
                 'rubber babies', 
@@ -48,24 +50,51 @@ function checkLetter(button) {
         if (ListLI[i].className==="letter" && button.textContent === ListLI[i].textContent) {
             //when match is found, letter li gets "show" class & match = letter
             ListLI[i].classList.add("show");
+            ListLI[i].style.transition = "all .5s";
             match = button.textContent;
-            //return match, exit loop and function
-            return match; 
         } 
     }
     //if no match found, return match = null, exit function
     return match;
 }
 
+// checks if user wins or loses by checking shown letter length, or missed count
+function checkWin() {
+    const letter = document.getElementsByClassName("letter");
+    let show = document.getElementsByClassName("show");
+
+    if (letter.length === show.length) {
+        overlay.classList.add("win");
+        heading.textContent = "Congrats, you won!"
+        overlay.style.display = "flex";
+    } else if (missed >= 5) {
+        overlay.classList.add("lose");
+        heading.textContent = "You lose..."
+        overlay.style.display = "flex";
+    }
+}
+
 //listen for the "Start Game" button to be clicked
 buttonReset.addEventListener('click', () => {
     overlay.style.display = "none";
+    //if (buttonReset.textContent === "")
     });
 
 //listen for onscreen keyboard clicks
 qwerty.addEventListener("click", (e) => {
-    //do stuff
+    if (e.target.tagName === 'BUTTON' && !e.target.classList.contains('chosen')) {
+        e.target.classList.add("chosen");
+        e.target.disabled=true;
+        let letterFound = checkLetter(e.target);
+        if (!letterFound) {
+            scoreboard.removeChild(scoreboard.lastElementChild);
+            missed++;
+        }
+    }
+    checkWin();
 });
+
+
 
 const phraseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
